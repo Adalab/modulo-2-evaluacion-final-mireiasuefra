f91fc78acf435421555965fc53a59b11dfe48187
mireiasuefra <mireia.suefra@gmail.com>
2021-12-28 18:41:31 +0100

    listener function for the search button and the series is painted in the li of the code.

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,61 @@
 
 console.log('>> Ready :)');
 
+// -- Variables -- //
+//arrays//
+let series = [];
+let favouriteSeries = [];
+//resto de variables//
+const apiUrl = 'https://api.jikan.moe/v3/search/anime';
+const btnSearch = document.querySelector('.js-btn-search');
+const listSeries = document.querySelector('.js-list-series');
+const inputSearch = document.querySelector('.js-search_series');
+
+
+
+// -- Funciones -- //
+
+//traen codigo del html//
+function getHtmlSerie(serie) {
+
+  // podría hacer una variable para meterla en imagen, igual con el ||. probar.
+  let htmlCode = `<li class="list-series">`;
+  htmlCode += `  <h2 class="title-series">${serie.title}</h2>`;
+  htmlCode += `  <img class="img-series js-img-series" src="${serie.image_url}"`;
+  htmlCode += `    alt="no existe imagen">`;
+  htmlCode += `</li>`;
+
+  return htmlCode;
+}
+
+// pintan //
+function paintListSeries() {
+  if (series) {
+    for (let index = 0; index < series.length; index++) {
+      const serie = series[index];
+      listSeries.innerHTML += getHtmlSerie(serie);
+    }
+  } else {
+    listSeries.innerHTML = 'serie no encontrada, prueba con otra'; // si me da tiempo lo meto en otro lado, ahora lo tengo en el ul.
+  }
+}
+
+
+ 
+
+//--Fetch--//
+function searchSeries(ev) {
+  ev.preventDefault();
+  const searchText = inputSearch.value;
+
+  fetch(`${apiUrl}?q=${searchText}`)
+    .then((response) => response.json())
+    .then((data) => {
+      series = data.results;
+      paintListSeries();
+    });
+}
+
+// -- Listener -- //
+
+btnSearch.addEventListener('click', searchSeries);
